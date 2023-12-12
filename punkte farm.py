015b747c0a100d65bdb261928167d7e4adf0a90c
sage071920 <93936545+sage071920@users.noreply.github.com>
2023-12-12 20:53:58 +0100

    Add files via upload

--- punkte farm.py
+++ punkte farm.py
@@ -4,18 +4,63 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+import re
+from discord_webhook import DiscordWebhook
 
-#Am anfang noch selber in Mündlich rein gehen ansonsten geht es von selber solange es immer 100 Wörter sind die bei Mündlich sind 
+def click_solution_and_correct_buttons(url1):
 
-def click_solution_and_correct_buttons(url):
-    options = Options()
-    options.add_argument("--user-data-dir=/path/to/profile")
 
-    driver = webdriver.Chrome(options=options)
+    driver.get(url1)
+    username_field = driver.find_element(By.NAME, "name")
+    password_field = driver.find_element(By.NAME, "password")
 
-    driver.get(url)
+    username_field.send_keys("USERNAME")
+    password_field.send_keys("PASSWORD")
+
+    login_button = driver.find_element(By.NAME, "submit")
+    login_button.click()
+    
+    desired_text = "Vokabeltrainer n. Karteikasten"
+        
+    div_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//div[text()='{desired_text}']"))
+        )
+
+    div_element.click()
+
+    time.sleep(1)
+
+    Muendlich_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//label[contains(text(), "Mündlich")]'))
+        )
+
+    Muendlich_button.click()
+
+    time.sleep(1)
+
+    Starten_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Starten")]'))
+        )
+
+    Starten_button.click()
+
     while True:
-        for i in range(100):
+
+        time.sleep(1.5)
+        xpathdiv = '//div[@class="col-md-5"]'
+        div_textzahl = driver.find_element(By.XPATH, xpathdiv)
+        text = div_textzahl.text
+        zahl = re.search(r'\d+', text)
+
+        if zahl:
+            extracted_number = int(zahl.group())  
+            print(extracted_number) 
+ 
+        else:
+         print("Keine Zahl gefunden")
+
+
+        for i in range(extracted_number):
             loesung_button = WebDriverWait(driver, 90).until(
                 EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Lösung")]'))
             )
@@ -30,45 +75,60 @@ def click_solution_and_correct_buttons(url):
 
             richtig_button.click()
 
-        speichern_button = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "speichern")]'))
-        )
-
-        speichern_button.click()
-
         time.sleep(1)
 
-        Hauptmenue_button = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Hauptmenü")]'))
-        )
+        if extracted_number < 100:
+            ending()
+        else:
+            stillgoing()
 
-        Hauptmenue_button.click()
+def stillgoing():
+    speichern_button2 = WebDriverWait(driver, 50).until(
+        EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Ergebnisse speichern")]'))
+    )
 
-        time.sleep(1)
+    speichern_button2.click()
+    time.sleep(1)
+    Hauptmenue_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Hauptmenü")]'))
+    )
+    Hauptmenue_button.click()
+    time.sleep(1)
+    desired_text = "Vokabeltrainer n. Karteikasten"
+    div_element=WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, f"//div[text()='{desired_text}']"))
+    )
 
-        desired_text = "Vokabeltrainer n. Karteikasten"
-        div_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, f"//div[text()='{desired_text}']"))
-        )
+    div_element.click()
+    time.sleep(1)
+    Muendlich_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//label[contains(text(), "Mündlich")]'))
+    )
+    Muendlich_button.click()
+    time.sleep(1)
+    Starten_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Starten")]'))
+    )
+    Starten_button.click()
 
-        div_element.click()
 
-        time.sleep(1)
 
-        Muendlich_button = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.XPATH, '//label[contains(text(), "Mündlich")]'))
-        )
+def ending():
+    speichern_button = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "speichern")]'))
+    )
+    speichern_button.click()
+    time.sleep(1)
+    webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1155481170868441189/mEYcyny46254WuOhSKjT73eXHoNbQ4o8lOilhXwccU2oyhvnZC0OEl1CQZhEUAXEDJIS", content="Navigium Fertig!!")
+    response = webhook.execute()
+    time.sleep(1)
+    exit()
 
-        Muendlich_button.click()
 
-        time.sleep(1)
+options = Options()
+options.add_argument("--user-data-dir=/path/to/profile")
 
-        Starten_button = WebDriverWait(driver, 50).until(
-            EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Starten")]'))
-        )
+driver = webdriver.Chrome(options=options)
 
-        Starten_button.click()
-
-url = ("https://www.navigium.de/login")
-
-click_solution_and_correct_buttons(url)
+url1 = ("https://www.navigium.de/login/schule/RBG_LANGENAU")
+click_solution_and_correct_buttons(url1)
